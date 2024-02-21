c22585a76e68f98cadfaf5bcbe0aae34c0dc4930
tyagiharsh7 <harsh.ttyagi281999@gmail.com>
2024-02-22 00:53:51 +0530

    feat: added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Analytics } from "@vercel/analytics/react"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <div className="h-screen w-screen bg-cover" style={{ backgroundImage: 'url("https://static.vecteezy.com/system/resources/previews/001/873/040/large_2x/abstract-modern-shape-black-gradient-geometric-stripes-diagonal-background-with-grunge-texture-free-vector.jpg")' }}> */}
-    <App />
-    {/* </div> */}
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+        <Analytics />
+    </React.StrictMode>
+);
